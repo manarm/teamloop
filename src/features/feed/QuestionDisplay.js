f023ed0325a0193423ce18cd03f16cd688925be1
manarm <mitchell@manarm.co>
2021-03-04 14:52:12 -0600

    quesiton block form style

--- src/features/feed/QuestionDisplay.js
+++ src/features/feed/QuestionDisplay.js
@@ -1,15 +1,21 @@
+import styles from './QuestionDisplay.module.scss';
+
 export default function QuestionDisplay({item, setAnswer, currentUser}) {
   const questionForm = (
-    <div onChange={e => setAnswer(e.target.value)}>
-      <input type="radio" id="yes" name="question" value="yes" />
-      <label htmlFor="yes">Yes</label>
-      <input type="radio" id="no" name="question" value="no" />
-      <label htmlFor="no">No</label>
+    <div className={styles.form} onChange={e => setAnswer(e.target.value)}>
+      <div className={styles.formRadio}>
+        <input type="radio" id="yes" name="question" value="yes" />
+        <label htmlFor="yes">Yes</label>
+      </div>
+      <div className={styles.formRadio}>
+        <input type="radio" id="no" name="question" value="no" />
+        <label htmlFor="no">No</label>
+      </div>
     </div>);
   const isAssignedToMe = item.assigned_to === currentUser;
   const displayForm = isAssignedToMe && item.status === 'NEW';
 
-  const answer = <div>Answer: {item.answer}</div>;
+  const answer = <div className={styles.answer}>Answer: {item.answer}</div>;
   const displayAnswer = item.status === 'COMPLETE';
 
   return (<div>
