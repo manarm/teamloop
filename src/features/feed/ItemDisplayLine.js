f1e6c4d0525f98e4a3f84d8aa954aad3adea4164
manarm <mitchell@manarm.co>
2021-03-04 11:27:15 -0600

    item line: tweak display

--- src/features/feed/ItemDisplayLine.js
+++ src/features/feed/ItemDisplayLine.js
@@ -1,41 +1,15 @@
 import styles from './ItemDisplayLine.module.scss'
-import { useState } from 'react';
 import { getNextStatus } from './feedSlice';
 import Button from '../common/Button';
 import Badge from '../common/Badge';
 
 export default function ItemDisplayLine({item, currentUser, setItemStatus, deleteItem, answerQuestion, onExpand}) {
-  const [questionState, setQuestionState] = useState(null);
+  const isAssignedToMe = item.assigned_to === currentUser;
   const next = getNextStatus(item);
 
-  const isQuestion = item.item_type === 'QUESTION';
-  const isAssignedToMe = item.assigned_to === currentUser;
-  const questionButtons = (
-  <span onChange={e => setQuestionState(e.target.value)}>
-    <input type="radio" id="yes" name="question" value="yes" />
-    <label htmlFor="yes">Yes</label>
-    <input type="radio" id="no" name="question" value="no" />
-    <label htmlFor="no">No</label>
-  </span>);
-  const displayQuestion = isQuestion && isAssignedToMe && item.status === 'NEW';
-
-  const questionAnswer = <>
-    Answer: {item.answerIsYes ? 'yes' : 'no'}
-  </>
-  const displayAnswer = isQuestion && item.status === 'COMPLETE';
-
   const handleNextClick = () => {
-    if(displayQuestion) {
-      if (questionState !== null) {
-        const answerIsYes = questionState === 'yes';
-        answerQuestion(item.id, answerIsYes);
-        setItemStatus(item.id, next.nextStatus);     
-      }
-    } else {
-      setItemStatus(item.id, next.nextStatus);
-    }
+    setItemStatus(item.id, next.nextStatus);
   }
-
   const displayNextControl = next !== null && isAssignedToMe;
 
   const displayTitle = item.title.length > 30 ?
@@ -44,26 +18,20 @@ export default function ItemDisplayLine({item, currentUser, setItemStatus, delet
 
   return (
   <div className={styles.wrapper}>
-  <button onClick={onExpand} className={styles.titleButton}>
-    <Badge type={item.item_type} />
-    <span className={styles.title}>
-      {displayTitle}
+    <button onClick={onExpand} className={styles.titleButton}>
+      <Badge type={item.item_type} />
+      <span className={styles.title}>
+        {displayTitle}
+      </span>
+    </button>
+    <span className={styles.user}>{`to: ${item.assigned_to}`}</span>
+    <span className={styles.user}>{`from: ${item.author}`}</span>
+    <span className={styles.controls}>
+      {displayNextControl && (
+        <Button onClick={handleNextClick}>{next.verb}</Button>
+      )}
+      <Button onClick={onExpand}>Expand</Button>
     </span>
-  </button>
-  <span>
-    <div>{`by: ${item.author}`}</div>
-    <div>{`to: ${item.assigned_to}`}</div>
-  </span>
-  <span>
-    { displayQuestion && questionButtons }
-    { displayAnswer && questionAnswer }
-  </span>
-  <span className={styles.controls}>
-    <Button onClick={onExpand}>Expand</Button>
-    {displayNextControl && (
-      <Button onClick={handleNextClick}>{next.verb}</Button>
-    )}
-  </span>
   </div>
   );
 }
