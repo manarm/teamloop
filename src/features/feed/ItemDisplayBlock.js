0360b0120ba24f3538b1ee4d1634db1a7abff3f9
manarm <mitchell@manarm.co>
2021-03-04 11:47:55 -0600

    item block styling

--- src/features/feed/ItemDisplayBlock.js
+++ src/features/feed/ItemDisplayBlock.js
@@ -8,10 +8,11 @@ export default function ItemDisplayBlock({item, onClose}) {
     <div className={styles.title}><h3>{item.title}</h3></div>
     <div className={styles.close}><Button onClick={onClose}>Close</Button></div>
     <div className={styles.meta}><ul>
-      <li>userinfo</li>  
-      <li>etc</li>  
+      <li>from: {item.author}</li>  
+      <li>to: {item.assigned_to}</li>  
+      <li>status: {item.status.toLowerCase()}</li>  
     </ul></div>
-    <div className={styles.content}>MAIN CONTENT</div>
+    <div className={styles.content}>Description here.</div>
     <div className={styles.controls}>controls</div>
   </div>;
 }
